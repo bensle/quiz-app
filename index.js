dff89d5f1d344a152b8c19644882e1e74495f52d
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-16 23:08:40 +0200

    Add first JavaScript

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 console.clear();
 
-const navClick = document.querySelector('[data-js="navicon"]');
-console.log(navClick);
+const bookmarkCard = document.querySelector('[data-js="bookmarkme"'); //creating the variable
+console.log(bookmarkCard); //output in console - see if the set value was found
+
+function onClickBookmark() {
+  //creating the function
+  alert("bookmarked");
+}
+
+bookmarkCard.addEventListener("click", onClickBookmark); //using the created function "onClickBookmark" which will send an alert with "bookmarked" when the set variable has been clicked
