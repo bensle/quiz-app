ff74b0da9b048f4aad8c2ba4e3e44d8a2f1fdc60
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-19 14:13:07 +0200

    Linting Basics

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log(bookmarkCard); //output in console - see if the set value was found
 
 function onClickBookmark() {
   //creating the function
-  alert("bookmarked");
+  alert('bookmarked');
 }
 
-bookmarkCard.addEventListener("click", onClickBookmark); //using the created function "onClickBookmark" which will send an alert with "bookmarked" when the set variable has been clicked
+bookmarkCard.addEventListener('click', onClickBookmark); //using the created function "onClickBookmark" which will send an alert with "bookmarked" when the set variable has been clicked
