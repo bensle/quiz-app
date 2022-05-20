b0bd48422ca486b1f6d884ce1e7b04374e2ff15c
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-20 13:02:26 +0200

    Add navigation js, name datajs, structure

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 console.clear();
+// Page-Variablen
+const homePage = document.querySelector('[data-js="indexpage"]');
+const bookmarksPage = document.querySelector('[data-js=bookmarksPage"]');
+const createCardPage = document.querySelector('[data-js="createCardPage"');
+const profilePage = document.querySelector('[data-js="profilePage"');
 
-const bookmarkCard = document.querySelector('[data-js="bookmarkme"'); //creating the variable
-console.log(bookmarkCard); //output in console - see if the set value was found
-
-function onClickBookmark() {
-  //creating the function
-  alert('bookmarked');
-}
-
-bookmarkCard.addEventListener('click', onClickBookmark); //using the created function "onClickBookmark" which will send an alert with "bookmarked" when the set variable has been clicked
+// Navigation-Buttons-Variablen
+const homePageButton = document.querySelector('[data-js="navigation__home"]');
+const bookmarksButton = document.querySelector(
+  '[data-js="navigation__bookmarks"]'
+);
+const createCardButton = document.querySelector(
+  '[data-js="navigation__createcard"]'
+);
+const profileButton = document.querySelector('[data-js="navigation__profile"]');
