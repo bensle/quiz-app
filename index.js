3a0b1e9dbc9d8a6b9f3dbf2c13c34bdab92e3626
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-20 17:41:23 +0200

    Add ja for all bookmarks

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ const createCardButton = document.querySelector(
 );
 const profileButton = document.querySelector('[data-js="navigation__profile"]');
 
+// Get all BookmarkCardButtons
+const bookmarkCardButtons = document.querySelectorAll('[data-js="bookmarkme"');
+
+bookmarkCardButtons.forEach(bookmarkButton => {
+  bookmarkButton.addEventListener('click', () => {
+    bookmarkButton.classList.toggle('card__bookmark--active');
+  });
+});
+
 // EventListeners Navigation hidepage
 homePageButton.addEventListener('click', () => {
   bookmarksPage.classList.add('hidepage');
