f9d03fb5870f389ec8f589ce6b0c35ffbdbc704a
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-20 14:18:06 +0200

    Add navigation hidesections and activebutton

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.clear();
 // Page-Variablen
 const homePage = document.querySelector('[data-js="indexpage"]');
-const bookmarksPage = document.querySelector('[data-js=bookmarksPage"]');
+const bookmarksPage = document.querySelector('[data-js="bookmarksPage"]');
 const createCardPage = document.querySelector('[data-js="createCardPage"');
 const profilePage = document.querySelector('[data-js="profilePage"');
 
@@ -14,3 +14,60 @@ const createCardButton = document.querySelector(
   '[data-js="navigation__createcard"]'
 );
 const profileButton = document.querySelector('[data-js="navigation__profile"]');
+
+// EventListeners Navigation hidepage
+homePageButton.addEventListener('click', () => {
+  bookmarksPage.classList.add('hidepage');
+  createCardPage.classList.add('hidepage');
+  profilePage.classList.add('hidepage');
+  homePage.classList.remove('hidepage');
+});
+
+bookmarksButton.addEventListener('click', () => {
+  bookmarksPage.classList.remove('hidepage');
+  createCardPage.classList.add('hidepage');
+  profilePage.classList.add('hidepage');
+  homePage.classList.add('hidepage');
+});
+
+createCardButton.addEventListener('click', () => {
+  bookmarksPage.classList.add('hidepage');
+  createCardPage.classList.remove('hidepage');
+  profilePage.classList.add('hidepage');
+  homePage.classList.add('hidepage');
+});
+
+profileButton.addEventListener('click', () => {
+  bookmarksPage.classList.add('hidepage');
+  createCardPage.classList.add('hidepage');
+  profilePage.classList.remove('hidepage');
+  homePage.classList.add('hidepage');
+});
+
+// EventListeners Navigation Button active
+
+homePageButton.addEventListener('click', () => {
+  homePageButton.classList.add('navigation__button--active');
+  bookmarksButton.classList.remove('navigation__button--active');
+  createCardButton.classList.remove('navigation__button--active');
+  profileButton.classList.remove('navigation__button--active');
+});
+
+bookmarksButton.addEventListener('click', () => {
+  homePageButton.classList.remove('navigation__button--active');
+  bookmarksButton.classList.add('navigation__button--active');
+  createCardButton.classList.remove('navigation__button--active');
+  profileButton.classList.remove('navigation__button--active');
+});
+createCardButton.addEventListener('click', () => {
+  homePageButton.classList.remove('navigation__button--active');
+  bookmarksButton.classList.remove('navigation__button--active');
+  createCardButton.classList.add('navigation__button--active');
+  profileButton.classList.remove('navigation__button--active');
+});
+profileButton.addEventListener('click', () => {
+  homePageButton.classList.remove('navigation__button--active');
+  bookmarksButton.classList.remove('navigation__button--active');
+  createCardButton.classList.remove('navigation__button--active');
+  profileButton.classList.add('navigation__button--active');
+});
