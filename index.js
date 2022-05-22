dafe3a2fe9815d6697485c53857b3ad3f900b31f
Benjamin Scheffler <scheffler-benjamin@web.de>
2022-05-22 15:02:41 +0200

    Fixed JS cardshow-nav-bookmark conflict

--- index.js
+++ index.js
@@ -8,43 +8,15 @@ const profilePage = document.querySelector('[data-js="profilePage"');
 // Show answers of individual cards-------------------------------------
 // targets the container of the hidden answer and the showAnswer Button
 const cardAnswerContainer = document.querySelectorAll('.card__show__container');
-console.log(cardAnswerContainer);
 
 // takes the container of cardanswer and button, followed by a queryselector for button and text in just that 1 container and add a eventListener to the button inside of that container that toggles the text in that container
 cardAnswerContainer.forEach(container => {
-  const showButton = container.querySelector('[data-js="card__showButton"]');
-  console.log(showButton);
-  const showText = container.querySelector('[data-js="showAnswerText"]');
-  console.log(showText);
+  const showButton = container.querySelector('.card__button');
+  const showText = container.querySelector('.card__showanswertext');
   showButton.addEventListener('click', () => {
     showText.classList.toggle('hideanswer');
   });
 });
-//---------------------------------------------------------------------
-
-// old function - toggles all answers --
-// function showOneText() {
-//   showAnswerText.forEach(paragraph => {
-//     paragraph.classList.toggle('hideanswer');
-//   });
-// }
-
-// showAnswerButton.forEach(showAnswer => {
-//   showAnswer.addEventListener('click', showOneText);
-// });
-
-// old function toggles just the first answer
-// showAnswerButton.forEach(showAnswer => {
-//   showAnswer.addEventListener('click', () => {
-//     showAnswerText.classList.toggle('hideanswer');
-//   });
-// });
-
-// CardButtons - old - const for old functions that just toggles the first or all answers
-// const showAnswerText = document.querySelectorAll('[data-js="showAnswerText"]');
-// const showAnswerButton = document.querySelectorAll(
-//   '[data-js="card__showButton"]'
-// );
 
 // Navigation-Buttons-Variablen
 const homePageButton = document.querySelector('[data-js="navigation__home"]');
@@ -131,3 +103,29 @@ profileButton.addEventListener('click', () => {
   createCardButton.classList.remove('navigation__button--active');
   profileButton.classList.add('navigation__button--active');
 });
+
+//---------------------------------------------------------------------
+// old function - toggles all answers ---------------------------------
+// function showOneText() {
+//   showAnswerText.forEach(paragraph => {
+//     paragraph.classList.toggle('hideanswer');
+//   });
+// }
+
+// showAnswerButton.forEach(showAnswer => {
+//   showAnswer.addEventListener('click', showOneText);
+// });
+
+// old function toggles just the first answer
+// showAnswerButton.forEach(showAnswer => {
+//   showAnswer.addEventListener('click', () => {
+//     showAnswerText.classList.toggle('hideanswer');
+//   });
+// });
+
+// CardButtons - old - const for old functions that just toggles the first or all answers
+// const showAnswerText = document.querySelectorAll('[data-js="showAnswerText"]');
+// const showAnswerButton = document.querySelectorAll(
+//   '[data-js="card__showButton"]'
+// );
+//---------------------------------------------------------------------
